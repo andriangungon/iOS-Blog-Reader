de13ccf0f5ba1c69aac17fd6e71c6733abe2d18a
winter.melon <andriangungon@gmail.com>
2014-02-22 11:44:18 +0800

    added data for table
    i added property on MasterViewController.h and I hardcoded sample data
    in the implementation

--- BlogReader/BlogReader/MasterViewController.m
+++ BlogReader/BlogReader/MasterViewController.m
@@ -21,6 +21,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.titlesArray = [NSArray arrayWithObjects:
+                        @"Getting started with Wordpress",
+                        @"Whitespace in Web Dsign: What It is and Why You Should Use It",
+                        @"Adaptive Images and Responsice SVGs - Treehouse Show Episode 15",
+                        @"Productivity is About Constraints and Concentration",
+                        @"A Guide to Becoming the Smartest Developer on the Panet",
+                        @"Teacher Spotlight: Zac Gordon",
+                        @"Do You Love What You Do?",
+                        @"Applying Normalize.css Reset - Quick Tip",
+                        @"How I Wrote a Book in 3 Days",
+                        @"Responsive Techniques, JavaScript MVC Frameworks, Firefox 16 | Treehouse Show Episode 14", nil];
 }
 
 - (void)didReceiveMemoryWarning
